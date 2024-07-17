8b930613ee0db9841916a11d9c2991507d6dce9d
TPKapgate <101078470+TPKapgate@users.noreply.github.com>
2024-07-17 19:41:47 +0530

    Update option.controller.js

--- src/option/option.controller.js
+++ src/option/option.controller.js
@@ -2,12 +2,14 @@ import { addVoteRepo, deleteoptRepo } from "./option.Repository.js";
 
 
 
+// Delete option
 export const deleteOpt = async(req,res)=>{
     const optID = req.params.id;
     const temp = await deleteoptRepo(optID);
     res.send(temp);
 }
 
+// ADd Vote
 export const addVote = async(req,res)=>{
     const optID = req.params.id;
     const temp = await addVoteRepo(optID);
