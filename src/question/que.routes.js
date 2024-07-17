6ff9da27f2882cb9ef2d0dc30bbb3b78e7fd62d5
TPKapgate <101078470+TPKapgate@users.noreply.github.com>
2024-07-17 19:35:17 +0530

    Update que.routes.js

--- src/question/que.routes.js
+++ src/question/que.routes.js
@@ -6,10 +6,10 @@ import { optRouter } from "../option/option.routes.js";
 
 export const queRouter = express.Router();
 
-queRouter.route("/create").post(createQuestion);
-queRouter.route("/create").get(throwError);
-queRouter.route("/:id/options/create").post(createOption);
-queRouter.route("/:id/delete").delete(deleteQue)
-queRouter.route("/:id").get(showQue)
+queRouter.route("/create").post(createQuestion);  //To create Que
+queRouter.route("/create").get(throwError); // Just to throw error as action should be post not get
+queRouter.route("/:id/options/create").post(createOption); // Create Option
+queRouter.route("/:id/delete").delete(deleteQue) //Delete Que
+queRouter.route("/:id").get(showQue) //Show Que
 
 
