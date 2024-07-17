f030a1954ac6349df0fb4d4d9cc50599b30a5559
TPKapgate <101078470+TPKapgate@users.noreply.github.com>
2024-07-17 19:41:17 +0530

    Update option.routes.js

--- src/option/option.routes.js
+++ src/option/option.routes.js
@@ -3,5 +3,5 @@ import { addVote, deleteOpt } from "./option.controller.js";
 
 export const optRouter = express.Router();
 
-optRouter.route("/:id/delete").delete(deleteOpt)
-optRouter.route("/:id/add_vote").get(addVote)
+optRouter.route("/:id/delete").delete(deleteOpt) // Delete option
+optRouter.route("/:id/add_vote").get(addVote)    // Add vote to option
